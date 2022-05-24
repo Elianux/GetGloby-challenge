4f7b2b45c758a7979fd7598caebc453b7f9b6059
elianux <elian.chiesa@gmail.com>
2022-05-24 04:12:34 -0300

    Fixed paths *

--- services/movies.ts
+++ services/movies.ts
@@ -1,12 +1,17 @@
 export const getMovies = async (host: any, s: string) => {
-  const res = await fetch(`http://${host}/api/movies?s=${s}`)
+  let protocol: string = 'https://'
+  if (host.includes('localhost')) protocol = 'http://'
+
+  const res = await fetch(`${protocol}${host}/api/movies?s=${s}`)
   const movies = await res.json()
   return movies
 }
 
-export const getMovie = async (host: string, i: string) => {
-  const res = await fetch(`http://${host}/api/movies/${i}`)
+export const getMovie = async (host: any, i: string) => {
+  let protocol: string = 'https://'
+  if (host.includes('localhost')) protocol = 'http://'
+
+  const res = await fetch(`${protocol}${host}/api/movies/${i}`)
   const movie = await res.json()
   return movie
 }
-
