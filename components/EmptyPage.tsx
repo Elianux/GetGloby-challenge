ba0cbafb567bc9ccf1bac2ac7eaeee14827866bb
elianux <elian.chiesa@gmail.com>
2022-05-23 22:37:12 -0300

    Fix escape character in EmptyPage component

--- components/EmptyPage.tsx
+++ components/EmptyPage.tsx
@@ -4,7 +4,7 @@ export default function EmptyPage() {
   return (
     <div className="h-screen mt-10 ">
       <Image src="/welcome.png" alt="Empty" width={300} height={300} />
-      <p className="text-white font-bold text-3xl">C'mon, type something</p>
+      <p className="text-white font-bold text-3xl">Cmon, type something</p>
     </div>
   )
 }
